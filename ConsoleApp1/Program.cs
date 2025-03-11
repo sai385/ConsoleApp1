c7e8d4598eb8ba605ca0400b125298239cb5e400
sai vinay <vinayganesula@gmail.com>
2025-03-11 14:04:50 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+         public  Program(int x)
+        {
+            Console.WriteLine("constructor is called");
+        }
+        void method1(int y)
+        {
+            Program p = new Program(20);
+            Console.WriteLine(y);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Program p1 = new Program(10);
+            p1.method1(100);
+            
+        }
+    }
+}
